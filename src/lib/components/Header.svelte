8cf5f38569da4ddfa64a7342fe53e8d11a88dec4
valiantlynx <gormerykombo@gmail.com>
2023-08-02 22:32:27 +0200

    updates

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,15 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	import logo from '$lib/images/texbab-logo.png';
+	import { cart } from '$lib/utils/stores';
+
+// Use the actual cart data from your app's state management
+let cartItems: any = [];
+$: cartItems = $cart; // Observe the cart store and update the cartItems variable
+
+function calculateTotal() {
+	return cartItems.reduce((total: any, item: any) => total + item.price * item.quantity, 0);
+}
 </script>
 
 <header>
@@ -28,26 +36,43 @@
 			<li aria-current={$page.url.pathname.startsWith('/menu') ? 'page' : undefined}>
 				<a href="/menu">menu</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/cart') ? 'page' : undefined}>
-				<a href="/cart">cart</a>
-			</li>
+
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
 	</nav>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+	<div class="flex-none corner">
+		<div class="dropdown dropdown-end">
+			<label tabindex="-1" class="btn btn-ghost btn-circle">
+				<div class="indicator">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+						stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+					</svg>
+					<span class="badge badge-sm indicator-item indicator-start indicator-bottom">{cartItems.length}</span>
+				</div>
+			</label>
+			<div tabindex="-1" class="mt-3 card card-compact dropdown-content w-52 bg-primary -100 shadow">
+				<div class="card-body">
+					<span class="font-bold text-lg">{cartItems.length} Items</span>
+					<span class="text-info">Subtotal: {calculateTotal().toFixed(2)} kr</span>
+					<div class="card-actions">
+						<a href="/cart" class="btn btn-primary btn-block">View cart</a>
+					</div>
+				</div>
+			</div>
+		</div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
+		
+
 	}
 
 	.corner {
