49803c5a17fae914816339aa87f68b2e51ec09ed
minfuel <106936397+minfuel@users.noreply.github.com>
2023-08-02 11:52:35 +0000

    Admin folder

---
+++ src/routes/admin/chef/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import OrderConfirmation from "$lib/components/OrderConfirmation.svelte";
+</script>
+
+<svelte:head>
+	<title>Chef orders overview</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="flex flex-col justify-center items-center flex-1">
+	<h2>Chef</h2>
+	<h1>Orders overview</h1>
+</section>
+
+<style>
+</style>
---
+++ src/routes/admin/rider/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import OrderConfirmation from "$lib/components/OrderConfirmation.svelte";
+</script>
+
+<svelte:head>
+	<title>Deliveries overview</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="flex flex-col justify-center items-center flex-1">
+	<h2>Rider</h2>
+	<h1>Deliveries overview</h1>
+</section>
+
+<style>
+</style>
