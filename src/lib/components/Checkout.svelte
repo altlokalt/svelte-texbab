a2c0cfb456ed71535333b29e3aec8b732f973251
valiantlynx <gormerykombo@gmail.com>
2023-08-03 19:31:32 +0200

    just need to add the payment functions

--- src/lib/components/Checkout.svelte
+++ src/lib/components/Checkout.svelte
@@ -6,10 +6,7 @@
 	import DeliveryForm from '$lib/components/DeliveryForm.svelte';
 	import PaymentMethod from '$lib/components/PaymentMethod.svelte';
 	import { patchPocketbase } from '$lib/utils/api';
-	
 
-  
-  
 	// Use the actual cart data from your app's state management
 	let cartItems: any = [];
 	$: cartItems = $cart; // Observe the cart store
@@ -17,104 +14,120 @@
 	// Use the actual order data from your app's state management
 	let orderItem: any = [];
 	$: orderItem = $order; // Observe the order store
-  
+
 	// Step control
 	let currentStep = 1;
 
 	// ordered
-	let ordered = false
-	let paid = false
-  
+	let ordered = false;
+	let paid = false;
+
 	// Track delivery details submission
 	let deliveryDetailsSubmitted = false;
-  
+
+	// Track payment method selection
+	let selectedPaymentMethod = '';
+
 	// Handle moving to the next step
 	function nextStep() {
-	  if (currentStep === 2 && !deliveryDetailsSubmitted) {
-		alert('Please submit delivery details first.');
-		return;
-	  }
-	  currentStep += 1;
+		if (currentStep === 2 && !deliveryDetailsSubmitted) {
+			alert('Please submit delivery details first.');
+			return;
+		}
+
+		if (currentStep === 3 && !selectedPaymentMethod) {
+			alert('Please select a payment method.');
+			return;
+		}
+
+		currentStep += 1;
 	}
-  
+
 	// Handle moving to the previous step
 	function prevStep() {
-	  currentStep -= 1;
+		currentStep -= 1;
 	}
-  
+
 	// Handle delivery details submission
 	function handleDeliveryDetailsSubmit() {
-	  // You can add further validation here if needed
-	  deliveryDetailsSubmitted = true;
+		// You can add further validation here if needed
+		deliveryDetailsSubmitted = true;
 	}
-  
+
+	// handle order status
+	function handleOrderStatus() {
+		ordered = true;
+	}
+
+	// handle payment status
+	function handlePaymentStatus() {
+		paid = true;
+	}
+
 	// Handle placing the order (you can add further logic here)
-	function placeOrder() {
-		handleSubmit();
-	  console.log('Order Placed', orderItem.id);
-	  goto('/order-confirmation');
+	async function placeOrder() {
+		await handleSubmit();
+		console.log('Order Placed', orderItem.id);
+		goto('/order-confirmation');
 	}
 
 	async function handleSubmit() {
-	  const status = {
-		ordered,
-		paid
-	  };
+		const status = {
+			ordered,
+			paid,
+		};
 
-	  try {
-		const menuItems = await patchPocketbase(`texbab_orders/records/${orderItem.id}`, status); // the actual endpoint for menu items in your Pocketbase
-	} catch (error) {
-		throw new Error('Failed to fetch menu items.');
+		try {
+			const menuItems = await patchPocketbase(`texbab_orders/records/${orderItem.id}`, status); // the actual endpoint for menu items in your Pocketbase
+		} catch (error) {
+			throw new Error('Failed to fetch menu items.');
+		}
 	}
-  
-	
-	}
-  </script>
-  
-  <section class="p-4">
+</script>
+
+<section class="p-4">
 	<h1 class="text-2xl font-semibold mb-4">Checkout</h1>
 	{#if cartItems.length > 0}
-	  {#if currentStep === 1}
-		<!-- Step 1: Cart Summary -->
-		<div>
-		  <OrderSummary {cartItems} />
-		  <button on:click={nextStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Next</button>
-		</div>
-	  {:else if currentStep === 2}
-		  <!-- Step 2: Delivery Information -->
-		  <DeliveryForm onDeliveryDetailsSubmit={handleDeliveryDetailsSubmit} />
-		  <div class="flex justify-between mt-4">
-			<button on:click={prevStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Previous</button>
-			<button on:click={nextStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Next</button>
-		  </div>
-	  {:else if currentStep === 3}
-		<!-- Step 3: Payment Method -->
-		<PaymentMethod />
-		<div class="flex justify-between mt-4">
-		  <button on:click={prevStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Previous</button>
-		  <button on:click={nextStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Next</button>
-		</div>
-	  {:else if currentStep === 4}
-		<!-- Step 4: Order Confirmation -->
-		<div>
-		  <h2 class="font-semibold text-lg mb-2">Order Summary</h2>
-		  <OrderSummary {cartItems} />
-		</div>
-		<div class="flex justify-between mt-4">
-		  <button on:click={prevStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Previous</button>
-		  <button on:click={placeOrder} class="bg-blue-500 text-white px-4 py-2 rounded-md">Place Order</button>
-		</div>
-	  {/if}
+		{#if currentStep === 1}
+			<!-- Step 1: Cart Summary -->
+			<div>
+				<OrderSummary {cartItems} />
+				<button on:click={nextStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Next</button>
+			</div>
+		{:else if currentStep === 2}
+			<!-- Step 2: Delivery Information -->
+			<DeliveryForm onDeliveryDetailsSubmit={handleDeliveryDetailsSubmit} />
+			<div class="flex justify-between mt-4">
+				<button on:click={prevStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Previous</button>
+				<button on:click={nextStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Next</button>
+			</div>
+		{:else if currentStep === 3}
+			<!-- Step 3: Payment Method -->
+			<PaymentMethod onOrderStatus={handleOrderStatus} onPaymentStatus={handlePaymentStatus} bind:selectedPaymentMethod={selectedPaymentMethod} />
+			<div class="flex justify-between mt-4">
+				<button on:click={prevStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Previous</button>
+				<button on:click={nextStep} class="bg-blue-500 text-white px-4 py-2 rounded-md cursor-not-allowed:opacity-50">Next</button>
+			</div>
+		{:else if currentStep === 4}
+			<!-- Step 4: Order Confirmation -->
+			<div>
+				<h2 class="font-semibold text-lg mb-2">Order Summary</h2>
+				<OrderSummary {cartItems} />
+			</div>
+			<div class="flex justify-between mt-4">
+				<button on:click={prevStep} class="bg-blue-500 text-white px-4 py-2 rounded-md">Previous</button>
+				<button on:click={placeOrder} class="bg-blue-500 text-white px-4 py-2 rounded-md">Place Order</button>
+			</div>
+		{/if}
 	{:else}
-	  <p class="text-center">Your cart is empty.</p>
+		<p class="text-center">Your cart is empty.</p>
 	{/if}
-  </section>
-  
-  <style>
+</section>
+
+<style>
 	/* You can customize the styles here as needed */
 	.grid {
-	  max-width: 1280px;
-	  margin: 0 auto;
+		max-width: 1280px;
+		margin: 0 auto;
 	}
-  </style>
-  
+</style>
--- src/lib/components/PaymentMethod.svelte
+++ src/lib/components/PaymentMethod.svelte
@@ -1,18 +1,79 @@
 <!-- PaymentMethod.svelte -->
-<script>
-    let selectedPaymentMethod = '';
-  
-    function handleSubmit() {
-      // You can use the selectedPaymentMethod to process the payment method information, e.g., sending it to the server or storing it in the state.
-      // For simplicity, we'll just log the selected payment method here.
-      console.log('Selected Payment Method:', selectedPaymentMethod);
+<script lang="ts">
+  // Declare a custom event
+export let onOrderStatus: any;
+export let onPaymentStatus: any;
+export let selectedPaymentMethod: any;
+
+// Function to handle payment method submission
+async function handleSubmit() {
+  // You can use the selectedPaymentMethod to determine which payment method was chosen
+  // and perform the necessary actions accordingly.
+  if (selectedPaymentMethod === 'credit_card') {
+    // Handle credit card payment using Stripe
+    try {
+      // Replace this with your actual Stripe payment handling code
+      const paymentResult = await processCreditCardPayment();
+      console.log('Credit Card Payment Result:', paymentResult);
+    } catch (error) {
+      console.error('Error processing credit card payment:', error);
     }
-  </script>
+  } else if (selectedPaymentMethod === 'paypal') {
+    // Handle PayPal payment
+    try {
+      // Replace this with your actual PayPal payment handling code
+      const paymentResult = await processPayPalPayment();
+      console.log('PayPal Payment Result:', paymentResult);
+    } catch (error) {
+      console.error('Error processing PayPal payment:', error);
+    }
+  }  else if (selectedPaymentMethod === 'vipps') {
+    // Handle vipps payment
+    try {
+      // Replace this with your actual vipps payment handling code
+      const paymentResult = await processVippsPayment();
+      console.log('vipps Payment Result:', paymentResult);
+    } catch (error) {
+      console.error('Error processing vipps payment:', error);
+    }
+  } else {
+    // alert('Please select a payment method.');
+    alert('Please select a payment method.');
+  }
+
+  // Emit custom events for order status and payment status
+  onOrderStatus(true);
+  onPaymentStatus(true);
+}
+
+// Replace these functions with your actual payment handling functions
+async function processCreditCardPayment() {
+  // Implement credit card payment handling using Stripe or other payment gateway
+  // Return the payment result or handle the payment response as needed
+  return { success: true, message: 'Credit Card Payment Successful' };
+}
+
+async function processPayPalPayment() {
+  // Implement PayPal payment handling
+  // Return the payment result or handle the payment response as needed
+  return { success: true, message: 'PayPal Payment Successful' };
+}
+
+async function processVippsPayment() {
+  // Implement vipps payment handling
+  // Return the payment result or handle the payment response as needed
+  return { success: true, message: 'vipps Payment Successful' };
+}
+</script>
   
   <div class="border p-4 mb-4">
     <h2 class="font-semibold text-lg mb-2">Payment Method</h2>
     <form on:submit|preventDefault={handleSubmit}>
       <div class="grid grid-cols-2 gap-4">
+        <label>
+          <input type="radio" bind:group={selectedPaymentMethod} value="vipps" required />
+          <span class="ml-2 font-semibold">Vipps</span>
+        </label>
         <label>
           <input type="radio" bind:group={selectedPaymentMethod} value="credit_card" required />
           <span class="ml-2 font-semibold">Credit Card</span>
