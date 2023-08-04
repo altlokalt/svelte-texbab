55c94972ee89d8a2954c0d74ee1e7e616f81f731
minfuel <106936397+minfuel@users.noreply.github.com>
2023-08-04 19:59:19 +0000

    vipps works somewhat

--- src/routes/api/createPayment/+server.ts
+++ src/routes/api/createPayment/+server.ts
@@ -1,24 +1,46 @@
 import { error } from '@sveltejs/kit';
+import { v4 as uuid } from 'uuid';
 
 export async  function GET({ url }: any) {
+        const response = await fetch('https://de3d-20-229-142-68.ngrok-free.app/api/processVippsPayment')
+        console.log(response);
    
-        const paymentUrl = 'https://apitest.vipps.no/accessToken/get';
+        const paymentUrl = 'https://apitest.vipps.no/epayment/v1/payments';
+        const accessToken = await fetch('/api/processVippsPayment');
+        const idempotencyKey = ''
         const clientId = 'ac1e327e-41b7-44dc-b573-1ba195cc75ee';
         const clientSecret = '_bFXM_SMeXGjjNa13HXuVq-vNI4=';
         const subscriptionKey = 'f9b577d8debe48c096dbc3531c9db16a';
         const merchantSerialNumber = '307288';
       
         // Prepare the request body, if needed
-        const requestBody = {}; // Replace this with your actual request body if required
-      
+        const requestBody = {
+          "amount": {
+            "value": 4500,
+            "currency": "NOK"
+          },
+          "paymentMethod": {
+            "type": "CARD"
+          },
+          "customer": {
+            "phoneNumber": 4748431734
+          },
+          "reference": "34c31926-18b5-4d70-b605-6326f090fbaf",
+          "userFlow": "WEB_REDIRECT",
+          "returnUrl": "https://altlokal.com/redirect?reference=34c31926-18b5-4d70-b605-6326f090fbaf",
+          "paymentDescription": "Purchase of socks"
+        };
+        
         // Prepare the headers
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
       
         // Get the access token
-        const tokenResponse = await fetch('https://apitest.vipps.no/accessToken/get', {
+        const tokenResponse = await fetch('https://apitest.vipps.no/epayment/v1/payments', {
           method: 'POST',
           headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Idempotency-Key': idempotencyKey,
             'client_id': clientId,
             'client_secret': clientSecret,
             'Ocp-Apim-Subscription-Key': subscriptionKey,
@@ -33,8 +55,8 @@ export async  function GET({ url }: any) {
   if (tokenResponse.ok) {
     // If the response status is 2xx (successful)
     const data = await tokenResponse.json(); // Parse the response body as JSON
-    console.log(data.access_token);
-    return new Response(JSON.stringify({ success: true, token:data.access_token,  message: 'Payment processed successfully' }), {
+    console.log(data);
+    return new Response(JSON.stringify({ success: true, redirect:data.access_token,  message: 'Payment processed successfully' }), {
       status: tokenResponse.status,
       headers: { 'Content-Type': 'application/json' },
     });
