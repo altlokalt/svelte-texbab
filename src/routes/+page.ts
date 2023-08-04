7af897f4cf76e8ed088d27295959b2448255f592
valiantlynx <gormerykombo@gmail.com>
2023-08-04 20:05:23 +0200

    for the production mode

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,13 +1,4 @@
-import { dev } from '$app/environment';
 import { getPocketbase } from '$lib/utils/api';
-
-// we don't need any JS on this page, though we'll load
-// it in dev so that we get hot module replacement
-export const csr = dev;
-
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
 
 // export async function load({ params }) {
 export async function load({ params }: { params: { slug: string } }) {
