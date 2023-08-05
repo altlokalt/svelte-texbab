fec6d305b5e7dd2f8da19d0fbeafa63ff5cbfcca
altlokalt <116967722+altlokalt@users.noreply.github.com>
2023-08-05 23:50:12 +0000

    order tracking setup files

--- src/routes/order-confirmation/+page.svelte
+++ src/routes/order-confirmation/+page.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
 	import OrderConfirmation from "$lib/components/OrderConfirmation.svelte";
+	// OrderTracking
+	import type { LatLngExpression } from 'leaflet';
+	import Leaflet from '$lib/components/map/Leaflet.svelte';
+	import Marker from '$lib/components/map/Marker.svelte';
+	import Popup from '$lib/components/map/Popup.svelte';
+
+	const initialView: LatLngExpression = [59.356131420076714, 10.438487595706526]; // Horten, Norway
+	const markerLocations: Array<LatLngExpression> = [
+		[59.35593987077508, 10.43815570209242] // Road right outside the restaurant
+	];
 </script>
 
 <svelte:head>
@@ -9,6 +19,18 @@
 
 <section class="flex flex-col justify-center items-center flex-1">
 	<OrderConfirmation />
+	<!--OrderTracking-->
+	<div class="w-full h-screen">
+		<Leaflet view={initialView} zoom={14}>
+			{#each markerLocations as latLng}
+				<Marker {latLng} width={70} height={70}>
+					<!-- ShipBit Icon -->
+					<img src="/delivery_guy.svg" alt="delivery guy riding a bike" />
+					<Popup>Like & Subscribe!</Popup>
+				</Marker>
+			{/each}
+		</Leaflet>
+	</div>
 </section>
 
 <style>
