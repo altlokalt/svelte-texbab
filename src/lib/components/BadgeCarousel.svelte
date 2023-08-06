0956ca73ce3c9f765a3e97cd432b0b96501b935b
valiantlynx <gormerykombo@gmail.com>
2023-08-06 03:19:24 +0200

    badge carousel

---
+++ src/lib/components/BadgeCarousel.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import BadgeCard from './ui/BadgeCard.svelte';
+    let badges = [
+        {
+            name: "Barne",
+            url: "/barne"
+        },
+        {
+            name: "Burger",
+            url: "/burger"
+        },
+        {
+            name: "Drikke",
+            url: "/drikke"
+        },
+        {
+            name: "Kebab",
+            url: "/kebab"
+        },
+        {
+            name: "Kylling",
+            url: "/kylling"
+        },
+        
+    ];
+
+</script>
+
+    <div class=" w-full flex overflow-x-auto">
+        {#each badges as badge}
+        <BadgeCard name={badge.name} url={badge.url} />
+    {/each}
+    
+    </div>
+ 
--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,11 +1,14 @@
 <script>
 	import Hero from "$lib/components/ui/Hero.svelte"
+	import BadgeCarousel from "./BadgeCarousel.svelte";
 	import MenuCarousel from "./MenuCarousel.svelte";
+
 </script>
 
 <section>
 	<Hero />
-	<MenuCarousel />
+	
+	<BadgeCarousel />
 </section>
 
 <style>
