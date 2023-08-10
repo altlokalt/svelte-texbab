aa9e82ae5fae23b4fdfea100c20e36b497941e03
valiantlynx <gormerykombo@gmail.com>
2023-08-10 14:09:09 +0200

    remove force login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,6 @@
 	import '../app.pcss';
 	import Footer from '$lib/components/Footer.svelte';
 
-	import { authPocketbase } from '$lib/utils/api'; // Adjust these imports accordingly
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-
-	onMount(async () => {
-		// pb
-		const res = await authPocketbase('sverre', 'sverresverre');
-		goto('/login');
-		console.log('res: ', res);
-	});
-
 </script>
 
 <div class="flex flex-col min-h-screen">
