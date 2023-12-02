389af72ca1df21a3469d82a35dbc8e3c9f1c2706
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-02 19:31:33 +0100

    gg

---
+++ setup.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=apps/svelte-texbab https://github.com/minfuel/svelte-texbab.git minfuel-turborepo --squash
+git subtree pull --prefix=apps/svelte-texbab https://github.com/minfuel/svelte-texbab.git minfuel-turborepo --squash
+git subtree push --prefix=apps/svelte-texbab https://github.com/minfuel/svelte-texbab.git minfuel-turborepo
