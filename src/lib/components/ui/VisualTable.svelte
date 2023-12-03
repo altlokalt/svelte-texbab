0c7377ed20ffc281ba3c1db90cafd6c57f849e0e
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-03 03:28:34 +0100

    order

--- src/lib/components/ui/VisualTable.svelte
+++ src/lib/components/ui/VisualTable.svelte
@@ -27,52 +27,77 @@
 </script>
 
 <div class="overflow-x-auto mx-auto border-primary">
-    <table class="table-auto w-full">
-        <!-- head -->
-        <thead>
-            <tr>
-                <th />
-                <th>id</th>
-                <th>status</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- row 1 -->
-            {#each data.items.filter((order) => !order.prepared) as order}
-                <tr>
-                    <th>
-                        <label>
-                            <input type="checkbox" class="checkbox" on:click={() => handleOrderPrepared(order.id, true)} />
-                        </label>
-                    </th>
-                    <td>
-                        <div class="flex items-center space-x-3">
-                            <div class="avatar">
-                                <div class="mask mask-squircle w-12 h-12">
-                                    <img src={`https://avatars.dicebear.com/api/adventurer-neutral/${order.firstName}.svg`} alt="Avatar" />
-                                </div>
-                            </div>
-                            <div>
-                                <div class="font-bold text-sm sm:text-base md:text-lg overflow-wrap break-words word-break break-all">{order.id}</div>
-                                <div class="text-xs sm:text-sm md:text-base opacity-50 overflow-wrap break-words word-break break-all">{order.firstName} {order.lastName}</div>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <span class={`badge ${order.prepared ? 'badge-success' : 'badge-warning'} badge-sm overflow-wrap break-words word-break break-all`}>
-                            {order.prepared ? 'Prepared' : 'Not Prepared'}
-                        </span>
-                    </td>
-                </tr>
-            {/each}
-        </tbody>
-        <!-- foot -->
-        <tfoot>
-            <tr>
-                <th />
-                <th>Id</th>
-                <th>Status</th>
-            </tr>
-        </tfoot>
-    </table>
+	<table class="table-auto w-full">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th />
+				<th>id</th>
+				<th>status</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- row 1 -->
+			{#each data.items.filter((order) => !order.prepared) as order}
+				<tr>
+					<th>
+						<label>
+							<input
+								type="checkbox"
+								class="checkbox"
+								on:click={() => handleOrderPrepared(order.id, true)}
+							/>
+						</label>
+					</th>
+					<td>
+						<div class="flex items-center space-x-3">
+							<div class="avatar">
+								<div class="mask mask-squircle w-12 h-12">
+									<img
+										src={`https://avatars.dicebear.com/api/adventurer-neutral/${order.firstName}.svg`}
+										alt="Avatar"
+									/>
+								</div>
+							</div>
+							<div>
+								<div
+									class="font-bold text-sm sm:text-base md:text-lg overflow-wrap break-words word-break break-all"
+								>
+									{order.id}
+								</div>
+								<div
+									class="text-xs sm:text-sm md:text-base opacity-50 overflow-wrap break-words word-break break-all"
+								>
+									{order.firstName}
+									{order.lastName}
+								</div>
+							</div>
+						</div>
+					</td>
+					<td>
+						<span
+							class={`badge ${
+								order.prepared ? 'badge-success' : 'badge-warning'
+							} badge-sm overflow-wrap break-words word-break break-all`}
+						>
+							{order.prepared ? 'Prepared' : 'Not Prepared'}
+						</span>
+					</td>
+				</tr>
+				{#if order.order_items}
+					{#each order?.order_items as food}
+						<p>{food.name}</p>
+					{/each}
+				{/if}
+			{/each}
+		</tbody>
+		<!-- foot -->
+		<tfoot>
+			<tr>
+				<th />
+				<th>Id</th>
+				<th>Status</th>
+			</tr>
+		</tfoot>
+	</table>
 </div>
